216cef0ce8a78151ec503780a08b4babfe64721b
tarasovalx <tarasovalx99@gmail.com>
2021-12-15 19:38:57 +0300

    Scripted auto-commit on change (2021-12-15 19:38:56) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Server.java
+++ src/main/java/ru/bmstu/lab4/Server.java
@@ -6,8 +6,7 @@ public class Server {
     private static final String ACTOR_SYSTEM_NAME = "system";
 
     public static void start(){
-
-
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+
     }
 }
