d77f134af31e0ae4b264b41c6e4ee50477b065c1
tarasovalx <tarasovalx99@gmail.com>
2021-12-15 19:24:32 +0300

    Scripted auto-commit on change (2021-12-15 19:24:32) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Server.java
+++ src/main/java/ru/bmstu/lab4/Server.java
@@ -1,6 +1,13 @@
 package ru.bmstu.lab4;
 
 import akka.actor.ActorSystem;
+
 public class Server {
-    
+    private static final String ACTOR_SYSTEM_NAME = "system";
+
+    public static void start(){
+
+
+        ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+    }
 }
